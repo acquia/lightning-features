ce43e9615704b3056f7bfb187c0a4feb51c09425
brantwynn <brant.wynn@acquia.com>
2015-05-08 11:20:33 -0500

    updated the update.sh with some docblock and forceful overwrites

--- update.sh
+++ update.sh
@@ -1,6 +1,18 @@
 #!/usr/bin/env bash
 
+# --------------------------------------------------------------
+# Ensure you are using the latest version of Lightning Features.
+# --------------------------------------------------------------
+# This script can only be run from the directory:
+# `/profiles/[my-profile]/modules/contrib/lightning_features`
+# It overwrites existing Libraries in the directory:
+# `/profiles/[my-profile]/libraries`
+# And it overwrites existing contrib modules in the directory:
+# `/profiles/[my-profile]/modules/contrib`
+# --------------------------------------------------------------
+
+# Usage: sh update.sh  
 drush make --no-core lightning_features.make -y
-cp -R sites/all/libraries/* ../../../libraries/.
-cp -R sites/all/modules/contrib/* ../.
+cp -fR sites/all/libraries/* ../../../libraries/.
+cp -fR sites/all/modules/contrib/* ../.
 rm -rf sites
