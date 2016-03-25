8fc8615259d1a53ac811009268cbd214c09b0779
Brant Wynn <brant.wynn@acquia.com>
2016-03-24 21:15:20 -0500

    Disable the features override check.

--- .drupal-ti/runners/lightning-behat/script.sh
+++ .drupal-ti/runners/lightning-behat/script.sh
@@ -16,11 +16,7 @@ lightning_header Running tests
 # Copy into place because it doesn't come with panopoly_test.
 mv -f "$TRAVIS_BUILD_DIR"/behat.travis.yml.dist .
 
-# If this isn't an upgrade, we test if any features are overridden.
-if [[ "$UPGRADE" == none ]]
-then
-	DRUSH_ARGS="--root=$DRUPAL_TI_DRUPAL_DIR --uri=$DRUPAL_TI_WEBSERVER_URL:$DRUPAL_TI_WEBSERVER_PORT" "$TRAVIS_BUILD_DIR"/scripts/check-overridden.sh
-fi
+#	DRUSH_ARGS="--root=$DRUPAL_TI_DRUPAL_DIR --uri=$DRUPAL_TI_WEBSERVER_URL:$DRUPAL_TI_WEBSERVER_PORT" "$TRAVIS_BUILD_DIR"/scripts/check-overridden.sh
 
 # This replaces environment vars from $DRUPAL_TI_BEHAT_YML into 'behat.yml'.
 drupal_ti_replace_behat_vars
